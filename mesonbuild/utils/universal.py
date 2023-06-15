7518f6895fce36104a5814ffaa749e450ae7966a
Eli Schwartz <eschwartz@archlinux.org>
2023-06-15 16:27:18 -0400

    Feature checks: fall back to reporting insufficiently portable features
    When projects do not specify a minimum meson version, we used to avoid
    giving them the benefit of the Feature checks framework. Instead:
    
    - warn for features that were added after the most recent semver bump,
      since they aren't portable to the range of versions people might use
      these days
    
    - warn for features that were deprecated before the upcoming semver
      bump, i.e. all deprecated features, since they aren't portable to
      upcoming semver-compatible versions people might be imminently upgrading
      to

--- mesonbuild/utils/universal.py
+++ mesonbuild/utils/universal.py
@@ -66,6 +66,7 @@ _U = T.TypeVar('_U')
 __all__ = [
     'GIT',
     'python_command',
+    'NoProjectVersion',
     'project_meson_versions',
     'SecondLevelHolder',
     'File',
@@ -169,10 +170,13 @@ __all__ = [
 ]
 
 
+class NoProjectVersion:
+    pass
+
 # TODO: this is such a hack, this really should be either in coredata or in the
 # interpreter
 # {subproject: project_meson_version}
-project_meson_versions = collections.defaultdict(str)  # type: T.DefaultDict[str, str]
+project_meson_versions: T.Dict[str, T.Union[str, NoProjectVersion]] = {}
 
 
 from glob import glob
